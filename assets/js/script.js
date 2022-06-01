ced739b0fb8539296d30743e808f5782e885fa71
Tyler <Tyl3r.s@outlook.com>
2022-06-01 14:10:29 -0400

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -220,8 +220,24 @@ var deleteTask = function (taskId) {
   saveTasks();
 };
 
-var saveTasks = function() {
+var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+var loadTasks = function () {
+  var savedTasks = localStorage.getItem("tasks");
+
+  if (!savedTasks) {
+    return false;
+  }
+
+  savedTasks = JSON.parse(savedTasks);
+
+  // loop through savedTasks array
+  for (var i = 0; i < savedTasks.length; i++) {
+    // pass each task object into the `createTaskEl()` function
+    createTaskEl(savedTasks[i]);
+  }
 }
 
 // Create a new task
